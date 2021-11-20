6c05efeab8dfcfea7ff64a974362e2f24663315d
Cinta Ramayanti <cinta.ramayanti@gmail.com>
2021-11-20 12:07:30 +0700

    Tugas View dan Delete

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriController;
+use App\Http\Controllers\CustomerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [DashboardController::class, 'index']);
+Route::get('/product', [ProductController::class, 'index'])->name('product');
+Route::get('/product/{id}', [ProductController::class, 'destroy'])->name('productDelete');
+Route::get('/tambah', [ProductController::class, 'create'])->name('tambahProduct');
+
+Route::get('/categori', [CategoriController::class, 'index'])->name('categori');
+Route::get('/categori/{id}', [CategoriController::class, 'destroy'])->name('categori.destroy');
+
+
+Route::get('/customer', [CustomerController::class, 'index'])->name('customer');
+Route::get('/customer/{id}', [CustomerController::class, 'destroy'])->name('customer.destroy');
+
